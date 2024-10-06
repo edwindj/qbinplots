270cab83764ee545a1e3e874c835b1d732a6bc61
Edwin de Jonge <edwindjonge@gmail.com>
2024-10-06 15:16:02 +0200

    updating

---
+++ R/qbin_plot.R
@@ -0,0 +1,33 @@
+qbin_plot <- function(l, ncol=NULL, y_scale_rm = FALSE){
+  structure(
+    l,
+    class="qbin_plot",
+    ncol = ncol,
+    y_scale_rm = y_scale_rm
+  )
+}
+
+#' @export
+print.qbin_plot <- function(x, ...){
+  y_scale_rm <- attr(x, "y_scale_rm")
+
+  if (isTRUE(y_scale_rm)){
+  } else {
+  }
+
+  pw <- Reduce(`+`, x)
+
+  ncol <- attr(x, "ncol")
+  if (!is.null(ncol)){
+    pw + plot_layout(ncol = ncol)
+  }
+
+  print(pw, ...)
+}
+
+#' @export
+`[.qbin_plot` <- function(x, i){
+  xs <- unclass(x)[i]
+  class(xs) <- class(x)
+  xs
+}
--- inst/tinytest/test_funq_plot.R
+++ inst/tinytest/test_funq_plot.R
@@ -1,5 +1,11 @@
 funq_plot(iris)
 
+funq_plot(iris, n = 20)
+
 funq_plot(iris, "Sepal.Width", auto_fill = TRUE)
 
 funq_plot(iris, "Sepal.Width", xlim = c(2,4))
+
+funq_plot(iris, "Sepal.Width", xmarker=3)
+
+funq_plot(iris, "Sepal.Width", qmarker=0.2)
