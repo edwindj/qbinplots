60de402b935117acadbb15fafafbc08c55a9e9dd
Edwin de Jonge <edwindjonge@gmail.com>
2024-11-09 10:35:43 +0100

    renaming project

---
+++ R/qbinplots-package.R
@@ -0,0 +1,16 @@
+#' qbinplots
+#'
+#' This package creates plots using quantile binning.
+#'
+#' @section plot types:
+#'
+#' - [qbin_boxplot()] creates quantile bin boxplots, highlighting the change in median between qbins.
+#' - [qbin_barplot()] creates quantile bin barplots highlighting the size of means of qbins.
+#' - [qbin_heatmap()] creates quantile bin heatmaps highlighting the distribution within the qbins.
+#' - [funq_plot()] creates a functional view of the data, plotting the median and interquartile range of numerical variables and level frequency of the other variables as a function of the `x` variable using quantile bins.
+"_PACKAGE"
+
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
